3a8bd961f006276a32be13cf3117d5dd4a62e66e
Yuval Kogman <nothingmuch@woobling.org>
2009-10-31 10:58:13 +0200

    Docs

--- lib/Sub/Call/Tail.pm
+++ lib/Sub/Call/Tail.pm
@@ -3,8 +3,6 @@ package Sub::Call::Tail;
 use strict;
 use warnings;
 
-use namespace::clean;
-
 require 5.008001;
 use parent qw(Exporter DynaLoader);
 use B::Hooks::OP::Check::EntersubForCV;
@@ -24,3 +22,47 @@ pop our @ISA;
 __PACKAGE__
 
 __END__
+
+=pod
+
+=head1 NAME
+
+Sub::Call::Tail - Tail calls without goto
+
+=head1 SYNOPSIS
+
+    use Sub::Call::Tail;
+
+    # instead of @_ = ( $object, @args ); goto $object->can("method")
+    tail $object->method(@args);
+
+    # instead of @_ = @blah; goto &foo
+    tail foo(@blah);
+
+=head1 DESCRIPTION
+
+This module provides a C<tail> modifier for subroutine and method calls that
+will cause the invocation to have the same semantics as C<goto &sub>.
+
+When the C<tail> modifier is compiled the inner subroutine call is transformed
+at compile time into a goto.
+
+=head1 SEE ALSO
+
+L<B::Hooks::OP::Check::EntersubForCV>
+
+=head1 VERSION CONTROL
+
+L<http://github.com/nothingmuch/Sub-Call-Tail>
+
+=head1 AUTHOR
+
+Yuval Kogman
+
+=head1 COPYRIGHT & LICENSE
+
+	Copyright (c) 2009 Yuval Kogman. All rights reserved
+	This program is free software; you can redistribute
+	it and/or modify it under the same terms as Perl itself.
+
+=
