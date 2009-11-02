b5327ff34f96dd4d5fd3f29ca3c5e44836bcbe30
Yuval Kogman <nothingmuch@woobling.org>
2009-11-02 14:44:51 +0200

    version bump

--- lib/Sub/Call/Tail.pm
+++ lib/Sub/Call/Tail.pm
@@ -7,7 +7,7 @@ require 5.008001;
 use parent qw(Exporter DynaLoader);
 use B::Hooks::OP::Check::EntersubForCV;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 $VERSION = eval $VERSION;
 
 our @EXPORT = our @EXPORT_OK = qw(tail);
